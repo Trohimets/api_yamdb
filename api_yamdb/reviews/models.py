297e0923030c557c2439416a70f20bd473f90bb3
mmjax <maxskgtr@gmail.com>
2022-02-02 18:03:04 +0300

    добавлена кастомная модель User с bio и role

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+USER = "user"
+MODERATOR = "moderator"
+ADMIN = "admin"
+ROLES = [
+    ("user", USER),
+    ("moderator", MODERATOR),
+    ("admin", ADMIN)
+]
+
+class User(AbstractUser):
+    bio = models.TextField(
+        blank=True,
+    )
+    role = models.CharField(
+        max_length=100,
+        choices=ROLES,
+        default=USER
+    )
+    
